ab53e7d994f516e642c0a23648e0bbe0d3c14e31
Iain R. Learmonth <irl@fsfe.org>
2019-09-07 10:48:50 +0100

    iclcd(8): identify compliant with license conditions

---
+++ usr.sbin/iclcd/iclcd.c
@@ -0,0 +1,160 @@
+/*
+ * iclcd - intentification compliant with license conditions daemon
+ *
+ * Written by Iain R. Learmonth <irl@fsfe.org> for the public domain.
+ */
+
+/*
+ * TODO:
+ *
+ * - check for root euid
+ * - actually fork off and be a daemon
+ * - allow sleep duration to be customisable
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <stdarg.h>
+
+#include <sys/times.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <net/bpf.h>
+#include <sys/socket.h>
+#include <net/if.h>
+
+__dead void
+fatal(char* msg)
+{
+	syslog(LOG_DAEMON | LOG_EMERG,
+	    "iclcd hit fatal error; you might want to turn off your radio");
+	syslog(LOG_DAEMON | LOG_ERR,
+	    "%s", msg);
+	exit(1);
+}
+
+__dead void
+usage(void)
+{
+	extern char *__progname;
+
+	fprintf(stderr, "usage: %s [-i interface]\n",
+	    __progname);
+	exit(1);
+}
+
+char *
+mycallsign()
+{
+	FILE    *mcp;
+	char    *call, *nl;
+	size_t  callsize = 0;
+	ssize_t calllen;
+
+	call = NULL;
+
+	if ((mcp = fopen("/etc/mycallsign", "r")) == NULL)
+		fatal("could not open /etc/mycallsign");
+	if ((calllen = getline(&call, &callsize, mcp)) != -1) {
+		if ((nl = strchr(call, '\n')) != NULL)
+			nl[0] = '\0';
+		return call;
+	}
+	fatal("could not read callsign from /etc/mycallsign");
+}
+
+int
+composeframe(char *buf, char *call)
+{
+	int framelen, calllen;
+
+	const char header[] = {
+	  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, // broadcast destination
+	  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // source filled by bpf
+	  0x44, 0x00,                         // ethertype
+	  0x00                                // version
+	};
+
+	memcpy(buf, header, 15);
+	framelen = 15; // prefix is 15 bytes
+	calllen = strlcpy(&buf[14], call, 25);
+	if (calllen > 25) {
+		fatal("callsign in /etc/mycallsign too long");
+	}
+	framelen = framelen + calllen - 1; // remove the NUL from end of call
+	return framelen;
+}
+
+int
+openbpf(char *interface)
+{
+	char buf[11];
+	int bpf, iflen;
+	struct ifreq bound_if;
+	
+	for (int i = 0; i < 99; i++)
+	{
+		sprintf(buf, "/dev/bpf%i", i);
+		bpf = open(buf, O_RDWR);
+	
+		if (bpf != -1)
+			break;
+	}
+
+	iflen = strlen(interface);
+	if (strlcpy(bound_if.ifr_name, interface, sizeof(bound_if.ifr_name))
+	    < iflen)
+		fatal("interface name too long");
+
+	if(ioctl(bpf, BIOCSETIF, &bound_if) > 0)
+		return -1;
+
+	return bpf;
+}
+
+int
+main(int argc, char **argv)
+{
+	int bpf;
+	char ch, *interface;
+
+	interface = "kiss0"; // default
+
+	while ((ch = getopt(argc, argv, "i:")) != -1) {
+		switch (ch) {
+		case 'i':
+			interface = optarg;
+			break;
+		default:
+			usage();
+			break;
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if ((bpf = openbpf(interface)) == -1)
+		fatal("failed to open bpf interface");
+
+	char framebuf[40];
+	char *call = mycallsign();
+	int framelen = composeframe(framebuf, call);
+	int unslept;
+
+	syslog(LOG_DAEMON | LOG_INFO,
+	    "started up (interface %s, callsign: %s)", interface, call);
+
+	for (;;) {
+		if (write(bpf, &framebuf, framelen) != framelen) {
+			syslog(LOG_DAEMON | LOG_EMERG,
+			    "failed to send ident frame, might want to unplug");
+			syslog(LOG_DAEMON | LOG_ERR,
+			    "failure reason: %m");
+		}
+		while ((unslept = sleep(60)) != 0);
+	}
+}
